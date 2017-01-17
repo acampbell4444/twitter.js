70dc8699afda68083e812cb0f78f381187b2a07a
Alan Campbell <alancampbell4444@gmail.com>
2017-01-16 18:53:09 -0800

    nunjucks functional

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const chalk = require('chalk');
+const nunjucks = require('nunjucks')
 const app = express();
+
+app.set('view engine', 'html'); // have res.render work with html files
+nunjucks.configure('views',{noCache: true}); // point nunjucks to the proper directory for templates
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
 
 app.use('/special',function (req, res, next) {
 	console.log(chalk.blue.bgRed.bold("You're in the Special Zone!"))
@@ -14,13 +20,14 @@ app.use(function (req, res, next) {
 
 app.get('/', function(req, res){
 	console.log(chalk.blue('server listening'));
-  	res.status(200).send('welcome to the root page!')
+	const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	const tacos = ['soft','hard','spicy']
+	res.render( 'index', {subtitle: 'this is a subtitle', title: 'Hall of Fame', people: people, tacos: tacos} );
 });
 
 app.get('/news', function(req, res){
   	console.log('server listening on the news page')
   	res.status(200).send('welcome to the news page!')
 });
-
 
 app.listen(3000)
