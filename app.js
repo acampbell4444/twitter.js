11e497778fad50d2cea26cb7235836dbbf4844f8
Alan Campbell <alancampbell4444@gmail.com>
2017-01-16 17:57:02 -0800

    express configured

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const chalk = require('chalk');
 const app = express();
 
+app.use('/special',function (req, res, next) {
+	console.log(chalk.blue.bgRed.bold("You're in the Special Zone!"))
+	res.status(200).send('welcome to the Special Zone!')
+	next();
+})
+app.use(function (req, res, next) {
+	console.log(chalk.green(req.method, req.path, res.statusCode)) 
+	next();
+})
+
 app.get('/', function(req, res){
-console.log('server listening')
-  res.status(200).send('welcome!')
+	console.log(chalk.blue('server listening'));
+  	res.status(200).send('welcome to the root page!')
+});
+
+app.get('/news', function(req, res){
+  	console.log('server listening on the news page')
+  	res.status(200).send('welcome to the news page!')
 });
 
 
